e7eb137fa5b17c8b7ccf7e62e935b3c909871ae6
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-05-21 17:46:39 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
 public class Main {
-
+static class Product{
+        //create a method of following defination
+        //public int product(int x, int y) {}
+      public int product(int x,int y){
+          System.out.println(x+" "+y+" First");
+       return 0;
+      }
+      //create a Overloaded method product of following defination
+      //public int product(int x, int y, int z) {}
+      public int product(int x,int y,int z){
+      System.out.println(x+" "+y+" "+z+" Second");
+      return 0;
+      }
+      //create a Overloaded method product of following defination
+      //public double product(double x, double y) {}
+      public double product(double x,double y){
+       System.out.println(x+" "+y+" Third");
+       return 0.00;
+      }
+    }
+    public static void main(String[] args) {
+        //create object of Product in Main function called p
+        Product p=n7);
+        p.product(5.7,6.2);
+        p.product(10,20);ew Product();
+        //call this method also from Main using Product class object p
+        p.product(5,6,
+    }
 }
