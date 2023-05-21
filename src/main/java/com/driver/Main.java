8b508a44d5e93b2ed91325874350e6bf7f87c2b7
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-05-21 17:55:06 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,20 +5,20 @@ public class Main {
         //create a method of following defination
         //public int product(int x, int y) {}
       public int product(int x,int y){
-          System.out.println(x+" "+y);
-       return 0;
+         // System.out.println(x+" "+y);
+      // return 0;
       }
       //create a Overloaded method product of following defination
       //public int product(int x, int y, int z) {}
       public int product(int x,int y,int z){
-      System.out.println(x+" "+y+" "+z);
-      return 0;
+      //System.out.println(x+" "+y+" "+z);
+      //return 0;
       }
       //create a Overloaded method product of following defination
       //public double product(double x, double y) {}
       public double product(double x,double y){
-       System.out.println(x+" "+y);
-       return 0.00;
+       //System.out.println(x+" "+y);
+      // return 0.00;
       }
     }
     public static void main(String[] args) {
